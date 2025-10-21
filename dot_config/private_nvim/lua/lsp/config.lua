da9470ca3fe29305057cc3243d37f294d558b173
Hubert Bugaj <lesny.rumcajs+github@gmail.com>
2025-10-21 11:30:10 +0200

    fix: add ipkg to ignored parsers (treesitter issue)

--- dot_config/private_nvim/lua/lsp/config.lua
+++ dot_config/private_nvim/lua/lsp/config.lua
@@ -1,5 +1,6 @@
 require 'nvim-treesitter.configs'.setup {
   ensure_installed = "all",    -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+  ignore_install = { "ipkg" },
   highlight = {
     enable = true,             -- false will disable the whole extension
     disable = { "c", "rust" }, -- list of language that will be disabled
